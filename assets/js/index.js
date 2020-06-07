9370a9e6fcc224efb2fe763e9e350f7f02870c9a
malo chaperon <chaperon.malo@gmail.com>
2020-06-07 22:55:19 +0200

    all

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+// scroll reveal anime
+const sr = ScrollReveal();
+
+sr.reveal('.text-hero', {
+	 delay: 0,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('.video-home-hero', {
+	 delay: 0.8,
+    duration: 2000,
+    distance: "200px",
+    origin: "bottom"
+});
+
+sr.reveal('.recent-title', {
+	 delay: 0,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('.recent-text', {
+	 delay: 0,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
+
+sr.reveal('.box-content', {
+	 delay: 0.5,
+    duration: 2000,
+    distance: "150px",
+    origin: "bottom"
+});
