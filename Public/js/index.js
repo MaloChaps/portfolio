552e04e13f615f11911aea2af4a99ba2104e13d8
malo chaperon <chaperon.malo@gmail.com>
2020-10-11 18:27:34 +0200

    all

---
+++ Public/js/index.js
@@ -0,0 +1,92 @@
+// scroll reveal anime
+const sr = ScrollReveal();
+
+// <!-- Hero header image -->
+sr.reveal('.h2-black', {
+	 delay: 0,
+    duration: 1000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.h1-black', {
+	 delay: 0,
+    duration: 1000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+
+
+
+sr.reveal('.h1-white', {
+	 delay: 0,
+    duration: 1000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.h3-black', {
+	 delay: 0,
+    duration: 1000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.para-primary-black', {
+	 delay: 0,
+    duration: 1000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+sr.reveal('.para-secondary-black', {
+	 delay: 0,
+    duration: 1000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.home-video-section', {
+	 delay: 0.2,
+    duration: 1000,
+    distance: "200px",
+    origin: "bottom"
+});
+
+sr.reveal('.home-video-banner', {
+	 delay: 0,
+    duration: 1000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.skills-section', {
+	 delay: 1,
+    duration: 1000,
+    distance: "200px",
+    origin: "bottom"
+});
+
+
+sr.reveal('.work-box-section', {
+	delay: 1,
+    duration: 3000,
+    distance: "250px",
+    origin: "bottom"
+});
+
+sr.reveal('.skills-sentence-box', {
+	delay: 2,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+
+
+
+
